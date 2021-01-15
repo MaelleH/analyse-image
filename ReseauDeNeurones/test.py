c612111e955732e3e7482ed1fadc4bb1fdf6f5ac
MaelleH <h.maelle.98@gmail.com>
2021-01-15 14:59:55 +0100

    results ok !!!

--- ReseauDeNeurones/test.py
+++ ReseauDeNeurones/test.py
@@ -1,12 +1,13 @@
 from keras.models import load_model
 import keras.preprocessing.image as image
-
+from shutil import copy
 import os
 import numpy as np
 
 model = load_model('first_try.h5')
 
 test_data_dir = 'data/test'
+result_data_dir = 'data/results'
 
 nb_test_samples = 0
 
@@ -17,11 +18,30 @@ for root, dirs, files in os.walk(test_data_dir):
     nb_test_samples += len(files)
 
 images = np.zeros((nb_test_samples, img_width, img_height, 3))
-
+images_path = []
 for root, dirs, files in os.walk(test_data_dir):
     for imageToLoad in files:
         img = image.load_img(os.path.join(root, imageToLoad), target_size=(img_width, img_height))
         images[i, :] = img
+        images_path.append(imageToLoad)
         i += 1
 
 classes_predictions = np.argmax(model.predict(images), axis=-1)
+
+for root, dirs, files in os.walk(result_data_dir):
+    for file in files:
+        os.remove(os.path.join(root, file))
+
+for i in range(len(classes_predictions)):
+    classe_result_dir = os.path.join(result_data_dir, str(classes_predictions[i]))
+    if not os.path.isdir(classe_result_dir):
+        os.mkdir(classe_result_dir)
+    copy(os.path.join(test_data_dir, images_path[i]), os.path.join(classe_result_dir, images_path[i]))
+
+
+print(classes_predictions)
+
+
+
+
+
